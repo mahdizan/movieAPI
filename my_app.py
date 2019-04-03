d290599edc29ee60fb438af9e28c0d5cedb162f0
Mah93150 <mahdizanfi@gmail.com>
2019-04-03 21:59:17 +0200

    prod2

--- my_app.py
+++ my_app.py
@@ -4,6 +4,14 @@ from pandas import read_csv
 app = Flask(__name__)
 
 @app.route('/')
+
+
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 def my_reco():
     df = read_csv(r'C:\Users\Mah\my_reco.csv', sep=',', encoding='utf8', low_memory=False)
     head = df.head()
